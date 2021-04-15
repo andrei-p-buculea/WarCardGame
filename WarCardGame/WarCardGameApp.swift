fd9fc18f3ed30d18f3322e0962501b9d19a92533
Andrei-Petrisor Buculea <andreipetrisor99@gmail.com>
2021-04-15 07:32:05 +0300

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Andrei-Petrisor Buculea on 15.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
